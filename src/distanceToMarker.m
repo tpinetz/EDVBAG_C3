47a59e7455258e61cbc103334daf387e6a4550bb
salmir <delalic.salmir@gmail.com>
2015-11-23 17:06:45 +0100

    Funktion in welcher die Features und gefunden und das cm/pixel Verhältnis ermittelt werden soll

---
+++ src/distanceToMarker.m
@@ -0,0 +1,29 @@
+function [ distanceToMarker ] = distanceToMarker( inputImage, marker, sizeOfMarker )
+% Diese Funktion soll die Entfernung zum Marker und somit das cm/pixel
+% Verhältnis ermitteln. Damit kann unter anderem auch die zurückgelegte
+% Distanz zur Kamera berechnet werden!
+
+% inputImage.. Bild mit Person & Marker
+% marker.. Bild vom Marker
+% sizeOfMarker.. Größe des Markers in cm (sollte quadratisch sein).
+
+marker = rgb2gray(imread('Assets/Marker0.jpg'));
+inputImage = rgb2gray(imread('Assets/testImg3.jpg'));
+
+pointsOfMarker = detectSURFFeatures(marker);
+pointsOfImage = detectSURFFeatures(inputImage);
+
+[ features1, valid_points1] = extractFeatures(marker, pointsOfMarker);
+[ features2, valid_points2] = extractFeatures(inputImage, pointsOfImage);
+
+indexPairs = matchFeatures(features1, features2);
+
+matchedPoints1 = valid_points1(indexPairs(:, 1), :);
+matchedPoints2 = valid_points2(indexPairs(:, 2), :);
+
+figure; showMatchedFeatures(inputImage, marker, matchedPoints1, matchedPoints2);
+
+
+% distanceToMarker = 0;
+
+end
