17a44b3c4073035c87846b47899531777d94c839
Thomas Pinetz <Thomas Pinetz>
2016-01-01 11:28:57 +0100

    Fixed my CCL implementation.

--- src/CCL.m
+++ src/CCL.m
@@ -1,10 +1,8 @@
 function [numComponents, components] = CCL(bw, neighbourhood)
-    numComponents = 1;
+    numComponents = 0;
     components = {};
     dim = size(bw);
-    visited = zeros(dim);
-    
-    
+    visited = zeros(dim);    
     
     for r = 1:dim(1)
         for c = 1:dim(2)
@@ -15,20 +13,23 @@ function [numComponents, components] = CCL(bw, neighbourhood)
                 continue 
             end
             visited(r,c) = 1;
-            neighbours = neighbourhood(bw,r,c);
+            neighbours3 = neighbourhood(bw,r,c);
             component = [r,c];
+            
+            while sum(size(neighbours3)) > 0
+                neighbours = neighbours3;
+                neighbours3 = [];
+                for i = neighbours'
+                    if visited(i(1),i(2)) == 0 
+                        visited(i(1),i(2)) = true;
 
-            component = [];
-            for i = neighbours
-                if visited(i(1),i(2)) == 0 
-                    visited(i(1),i(2)) = true;
-                    
-                    neighbours2 = neighbourhood(bw,i(1),i(2));
-                    neighbours = [neighbours; neighbours2];
-                    component = [component; i(1), i(2)]
-                end               
+                        neighbours2 = neighbourhood(bw,i(1),i(2));
+                        neighbours3 = [neighbours3; neighbours2];
+                        component = [component; i(1), i(2)];
+                    end               
+                end
             end
-            components{numComponents} = component;
+            components{numComponents + 1} = component;
             numComponents = numComponents + 1;
         end
     end
--- src/calculateDistance.m
+++ src/calculateDistance.m
@@ -15,21 +15,17 @@ function [averages, averages2, distInPx] = calculateDistance(A, B)
     averages2 = [0,0];
     ave = 0;
     ave2 = 0;
+    
+    [nc, cc] = CCL(bw, @neighbour8);
 
-    for x = 1 : dim(1)
-        for y = 1 : floor(size(bw,2)/2)
-            if(bw(x,y)) 
-                averages(1) = averages(1) + x;
-                averages(2) = averages(2) + y;
-                ave = ave + 1;
-            end
-        end
-        for y = ceil(size(bw,2)/2) : size(bw,2)
-            if(bw(x,y))
-                averages2 = averages2 + [x, y];
-                ave2 = ave2 + 1;
-            end
-        end
+    for point = cc{1}
+        averages = averages + [point(1), point(2)];
+        ave = ave + 1;
+    end
+        
+     for point = cc{2}
+        averages2 = averages2 + [point(1), point(2)];
+        ave2 = ave2 + 1;
     end
     
     if ave > 0
