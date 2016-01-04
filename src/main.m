1d4cd51bae9ab18a3866331dca7cdad92770fb9d
Andreas Seiwaldstätter <seiwi@gmx.net>
2016-01-04 18:07:51 +0100

    added new testImages where CCL does not work on my PC

--- src/main.m
+++ src/main.m
@@ -1,11 +1,11 @@
 function main()
-marker = rgb2gray(imread('Assets/Marker0.jpg'));
-inputImage = rgb2gray(imread('Assets/testimg4.jpg'));
+marker = rgb2gray(imread('Assets/Marker1.png'));
+inputImage = rgb2gray(imread('Assets/CCLfail02.jpg'));
 sizeOfMarker = 80;
 pixelInMM = distanceToMarker(inputImage,marker,sizeOfMarker)
 
-inputImage1 = imread('Assets/testimg1.png');
-inputImage2 = imread('Assets/testimg2.png');
+inputImage1 = imread('Assets/CCLfail01.jpg');
+inputImage2 = imread('Assets/CCLfail02.jpg');
 [first, second, distInPx] = calculateDistance(inputImage1,inputImage2);
 dist = distInPx * pixelInMM
 
